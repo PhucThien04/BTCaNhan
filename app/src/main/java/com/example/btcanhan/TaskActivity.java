2a152341b286f22b6f2f81b54922e3666a9e3fc6
PhucThien04 <phucthiensh@gmail.com>
2024-11-29 08:13:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/btcanhan/TaskActivity.java
@@ -0,0 +1,15 @@
+package com.example.btcanhan;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TaskActivity extends AppCompatActivity {
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
